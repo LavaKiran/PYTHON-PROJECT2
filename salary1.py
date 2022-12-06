9a4cc09f9979d896534c130a780935f50b1d6def
LavaKiran <119869827+LavaKiran@users.noreply.github.com>
2022-12-06 23:19:40 +0530

    Add files via upload

---
+++ salary1.py
@@ -0,0 +1,10 @@
+class employee:
+    def getdata(self):
+        self.name="John"
+        self.salary=30000
+        self.__age=30
+    def display(self):
+        print("Name=",self.name)
+        print("Salary=",self.salary)
+        print("Age=",self__age)
+        
